348418fe5140269a1fcfd8e899c95ab486b868f2
Andreas Dahlström <andreas@yoda.nu>
2023-04-07 12:15:48 +0200

    added some kind of IOS network support. needs to be tested

--- ios/Plugin/vyouPluginPlugin.m
+++ ios/Plugin/vyouPluginPlugin.m
@@ -4,5 +4,7 @@
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(vyouPluginPlugin, "vyouPlugin",
-           CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+        CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+        CAP_PLUGIN_METHOD(getConnectionStatus, CAPPluginReturnPromise);
+        CAP_PLUGIN_METHOD(removeAllListeners, CAPPluginReturnPromise);
 )
--- ios/Plugin/vyouPluginPlugin.swift
+++ ios/Plugin/vyouPluginPlugin.swift
@@ -15,4 +15,58 @@ public class vyouPluginPlugin: CAPPlugin {
             "value": implementation.echo(value)
         ])
     }
+
+    private var networkImpl: Network?
+
+    override public func load() {
+        CAPLog.print("Loading network plugin")
+        do {
+            networkImpl = try Network()
+            networkImpl?.statusObserver = { [weak self] status in
+                CAPLog.print(status.logMessage)
+                self?.notifyListeners("networkStatusChange", data: [
+                    "connected": status.isConnected,
+                    "connectionType": status.jsStringValue
+                ])
+            }
+        } catch let error {
+            CAPLog.print("Unable to start network monitor: \(error)")
+        }
+    }
+
+    @objc func getConnectionStatus(_ call: CAPPluginCall) {
+        let status = networkImpl?.currentStatus() ?? Network.Connection.unavailable
+        call.resolve(["connected": status.isConnected, "connectionType": status.jsStringValue])
+    }
+}
+
+extension Network.Connection {
+    internal var jsStringValue: String {
+        switch self {
+        case .cellular:
+            return "cellular"
+        case .wifi:
+            return "wifi"
+        case .unavailable:
+            return "none"
+        }
+    }
+    internal var isConnected: Bool {
+        switch self {
+        case .cellular, .wifi:
+            return true
+        case .unavailable:
+            return false
+        }
+    }
+    internal var logMessage: String {
+        switch self {
+        case .cellular:
+            return "Reachable via Cellular"
+        case .wifi:
+            return "Reachable via WiFi"
+        case .unavailable:
+            return "Not reachable"
+        }
+    }
 }
